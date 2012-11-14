0ad9d84a0385c14896ecea0b282ac044ec474e21
Pavel Milkevich <pmi@enonic.com>
2012-11-14 13:01:11 +0300

    CMS-526 New UI: Implement account app according to new design.
    added border: false to some components,
    admin-filter, admin-grid, admin-detail and admin-toolbar classes,
    reverted splitter

--- example/shared/view/BaseDetailsPanel.js
+++ example/shared/view/BaseDetailsPanel.js
@@ -1,10 +1,16 @@
 Ext.define('Admin.view.BaseDetailsPanel', {
     extend: 'Ext.panel.Panel',
     alias: 'widget.baseDetailsPanel',
+
+    cls: 'admin-detail',
+    border: false,
     autoScroll: true,
+
     defaults: {
-        cls: 'admin-panel'
+        cls: 'admin-panel',
+        border: false
     },
+
     initComponent: function () {
         this.tbar = {
             xtype: 'toolbar',
--- example/shared/view/BaseFilterPanel.js
+++ example/shared/view/BaseFilterPanel.js
@@ -1,8 +1,9 @@
 Ext.define('Admin.view.BaseFilterPanel', {
     extend: 'Ext.panel.Panel',
     alias: 'widget.baseFilterPanel',
+
+    cls: 'admin-filter',
     width: 205,
-    bodyCls: 'admin-panel admin-filter-panel',
     autoScroll: true,
     initComponent: function () {
         var me = this;
