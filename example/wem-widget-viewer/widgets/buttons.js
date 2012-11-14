3d6c9e9b916cf666ce965aee8ececcff45ded836
Thomas Andersen <tan@enonic.com>
2012-11-14 20:29:39 +0100

    Theming: Test all button combos

--- example/wem-widget-viewer/widgets/buttons.js
+++ example/wem-widget-viewer/widgets/buttons.js
@@ -11,85 +11,260 @@ function createButtonsContainer() {
             },
             {
                 xtype: 'container',
-                defaults: {
-                    margin: '0 10 0 0'
-                },
                 items: [
                     {
-                        xtype: 'button',
-                        text: 'Small',
-                        scale: 'small'
-                    },
-                    {
-                        xtype: 'button',
-                        text: 'Medium',
-                        scale: 'medium'
-                    },
-                    {
-                        xtype: 'button',
-                        text: 'Large',
-                        scale: 'large'
-                    },
-                    {
-                        xtype: 'button',
-                        text: 'Small w/menu',
-                        scale: 'small',
-                        menu: [
-                            {text: 'Item 1'},
-                            {text: 'Item 2'},
-                            {text: 'Item 3'},
-                            {text: 'Item 4'}
+                        xtype: 'container',
+                        items: [
+                            {
+                                xtype: 'component',
+                                html: '<h5>Standard</h5>'
+                            },
+                            {
+                                xtype: 'container',
+                                defaults: {
+                                    margin: '0 5 0 0'
+                                },
+                                items: [
+                                    {
+                                        xtype: 'button',
+                                        text: 'Small',
+                                        scale: 'small'
+                                    },
+                                    {
+                                        xtype: 'button',
+                                        text: 'Medium',
+                                        scale: 'medium'
+                                    },
+                                    {
+                                        xtype: 'button',
+                                        text: 'Large',
+                                        scale: 'large'
+                                    },
+                                ]
+                            }
                         ]
                     },
+
                     {
-                        xtype: 'cycle',
-                        showText: true,
-                        prependText: 'Small Cycle ',
-                        scale: 'small',
-                        menu: {
-                            items: [
-                                {
-                                    text: 'JSON',
-                                    checked: true
+                        xtype: 'container',
+                        items: [
+                            {
+                                xtype: 'component',
+                                html: '<h5>Menu Button</h5>'
+                            },
+                            {
+                                xtype: 'container',
+                                defaults: {
+                                    margin: '0 5 0 0'
                                 },
-                                {
-                                    text: 'XML'
+                                items: [
+                                    {
+                                        xtype: 'button',
+                                        text: 'Small w/menu',
+                                        scale: 'small',
+                                        menu: [
+                                            {text: 'Item 1'},
+                                            {text: 'Item 2'},
+                                            {text: 'Item 3'},
+                                            {text: 'Item 4'}
+                                        ]
+                                    },
+                                    {
+                                        xtype: 'button',
+                                        text: 'Medium w/menu',
+                                        scale: 'medium',
+                                        menu: [
+                                            {text: 'Item 1'},
+                                            {text: 'Item 2'},
+                                            {text: 'Item 3'},
+                                            {text: 'Item 4'}
+                                        ]
+                                    },
+                                    {
+                                        xtype: 'button',
+                                        text: 'Large w/menu',
+                                        scale: 'large',
+                                        menu: [
+                                            {text: 'Item 1'},
+                                            {text: 'Item 2'},
+                                            {text: 'Item 3'},
+                                            {text: 'Item 4'}
+                                        ]
+                                    }
+                                ]
+                            }
+                        ]
+                    },
+
+                    {
+                        xtype: 'container',
+                        items: [
+                            {
+                                xtype: 'component',
+                                html: '<h5>Cycle Button</h5>'
+                            },
+                            {
+                                xtype: 'container',
+                                defaults: {
+                                    margin: '0 5 0 0'
                                 },
-                                {
-                                    text: 'Text'
-                                }
-                            ]
-                        }
+                                items: [
+                                    {
+                                        xtype: 'cycle',
+                                        scale: 'small',
+                                        showText: true,
+                                        prependText: 'Small Cycle',
+                                        menu: {
+                                            items: [
+                                                {
+                                                    text: 'JSON',
+                                                    checked: true
+                                                },
+                                                {
+                                                    text: 'XML'
+                                                },
+                                                {
+                                                    text: 'Text'
+                                                }
+                                            ]
+                                        }
+                                    },
+                                    {
+                                        xtype: 'cycle',
+                                        scale: 'medium',
+                                        showText: true,
+                                        prependText: 'Medium Cycle ',
+                                        menu: {
+                                            items: [
+                                                {
+                                                    text: 'JSON',
+                                                    checked: true
+                                                },
+                                                {
+                                                    text: 'XML'
+                                                },
+                                                {
+                                                    text: 'Text'
+                                                }
+                                            ]
+                                        }
+                                    },
+                                    {
+                                        xtype: 'cycle',
+                                        scale: 'large',
+                                        showText: true,
+                                        prependText: 'Large Cycle ',
+                                        menu: {
+                                            items: [
+                                                {
+                                                    text: 'JSON',
+                                                    checked: true
+                                                },
+                                                {
+                                                    text: 'XML'
+                                                },
+                                                {
+                                                    text: 'Text'
+                                                }
+                                            ]
+                                        }
+                                    },
+                                ]
+                            }
+                        ]
                     },
+
                     {
-                        xtype: 'splitbutton',
-                        text: 'Small Split',
-                        menu: new Ext.menu.Menu({
-                            items: [
-                                {
-                                    text: 'Item 1'
+                        xtype: 'container',
+                        items: [
+                            {
+                                xtype: 'component',
+                                html: '<h5>Split Button</h5>'
+                            },
+                            {
+                                xtype: 'container',
+                                defaults: {
+                                    margin: '0 5 0 0'
                                 },
-                                {
-                                    text: 'Item 2'
-                                }
-                            ]
-                        })
+                                items: [
+                                    {
+                                        xtype: 'splitbutton',
+                                        text: 'Small Split',
+                                        scale: 'small',
+                                        menu: new Ext.menu.Menu({
+                                            items: [
+                                                {
+                                                    text: 'Item 1'
+                                                },
+                                                {
+                                                    text: 'Item 2'
+                                                }
+                                            ]
+                                        })
+                                    },
+                                    {
+                                        xtype: 'splitbutton',
+                                        text: 'Medium Split',
+                                        scale: 'medium',
+                                        menu: new Ext.menu.Menu({
+                                            items: [
+                                                {
+                                                    text: 'Item 1'
+                                                },
+                                                {
+                                                    text: 'Item 2'
+                                                }
+                                            ]
+                                        })
+                                    },
+                                    {
+                                        xtype: 'splitbutton',
+                                        text: 'Large Split',
+                                        scale: 'large',
+                                        menu: new Ext.menu.Menu({
+                                            items: [
+                                                {
+                                                    text: 'Item 1'
+                                                },
+                                                {
+                                                    text: 'Item 2'
+                                                }
+                                            ]
+                                        })
+                                    }
+                                ]
+                            }
+                        ]
                     },
+
                     {
-                        xtype: 'component',
-                        html: '<h4>Colors</h4>'
-                    },
-                    {
-                        xtype: 'button',
-                        ui: 'red',
-                        text: 'Red',
-                        scale: 'small'
-                    },
-                    {
-                        xtype: 'button',
-                        ui: 'grey',
-                        text: 'Grey',
-                        scale: 'small'
+                        xtype: 'container',
+                        items: [
+                            {
+                                xtype: 'component',
+                                html: '<h5>Colors</h5><div>ui: red, ui: grey</div>'
+                            },
+                            {
+                                xtype: 'container',
+                                defaults: {
+                                    margin: '0 5 0 0'
+                                },
+                                items: [
+                                    {
+                                        xtype: 'button',
+                                        ui: 'red',
+                                        text: 'Red',
+                                        scale: 'small'
+                                    },
+                                    {
+                                        xtype: 'button',
+                                        ui: 'grey',
+                                        text: 'Grey',
+                                        scale: 'small'
+                                    }
+                                ]
+                            }
+                        ]
                     }
                 ]
             }
